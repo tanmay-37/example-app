21949ded7dff7393089a63af110ef4f2ffdb31d8
tanmay-37 <talekartk@rknec.edu>
2023-11-25 05:25:03 +0530

    Add routes for veggies

---
+++ resources/views/veggies.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Vegetable Table Store</h1>
+<table border="3">
+    <tr>
+        <th>Vegetable Name</th>
+        <th>Price</th>
+    </tr>
+    <tr>
+        <td>Baigan</td>
+        <td>15</td>
+    </tr>
+    <tr>
+        <td>Bhindi</td>
+        <td>20</td>
+    </tr>
+    <tr>
+        <td>Aaloo</td>
+        <td>30</td>
+    </tr>
+    <tr>
+        <td>Gobhi</td>
+        <td>25</td>
+    </tr>
+
+</table>
+
+</body>
+</html>

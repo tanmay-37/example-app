4cea4cdd55410d2f13bf15842d6efaa7afe14eab
tanmay-37 <talekartk@rknec.edu>
2023-11-26 11:27:01 +0530

    created song-list.blade.php to render songs for data base

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Models\Song;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/veggies', function () {
+    return view('veggies');
+});
+Route::get('/veggies/{veggieName}', function (string $veggieName){
+	return $veggieName;
+})->whereIn('veggieName',['baigan','bhindi','aaloo','gobhi']);
+
+Route::get('/songs_static', function () {
+    return "Songs";
+});
+Route::get('/songs_static', function () {
+    $song = new Song();
+    $song->setTitle('With You');
+    return view('songs', [ 'song' => $song ]);
+});
+Route::get('/songs_static', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+  
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+  
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+  
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  });
+  Route::get('/songs', function () {
+    return view('songs', [ 'songs' => Song::all() ] );
+});
+Route::get('/songs_static', function () {
+    return view('songs_static');
+});
